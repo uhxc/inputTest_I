5b45aefdcca537bf5ba653722c9fa7670b82a1fb
migue <migue@DESKTOP-HRROA4E>
2022-03-17 05:56:41 -0300

    Adicionar arquivos de projeto.

---
+++ inputTest_I/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inputTest_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string[] vet = Console.ReadLine().Split(' ');
+
+            Console.WriteLine($"você digitou:\n{frase}\n{x}\n{y}\n{z}");
+            Console.WriteLine($"{vet[0]} {vet[1]} {vet[2]}");
+
+
+
+        }
+    }
+}
